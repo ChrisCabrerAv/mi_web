f8a5a8dc2b9a5ccd33f834342337e4ae9748760c
Christian Cabrera <chris.nicoc117@hotmail.com>
2022-02-26 13:42:12 -0300

    Finalizada extensión templates, comienza project

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(verbose_name="Título", max_length=200)
+    description = models.TextField(verbose_name="Descripción")
+    image = models.ImageField(verbose_name="Imagen")
+    created = models.DateTimeField(verbose_name="Fecha de creación", auto_now_add=True)
+    updated = models.DateTimeField(verbose_name="Última actualización", auto_now=True)
+
+    class Meta:
+        verbose_name = "proyecto"
+        ordering = ['-created']
