4b238c36afaf41e29e00d7828f101a0c64aaca94
Christian Cabrera <chris.nicoc117@hotmail.com>
2022-03-04 09:59:59 -0300

    portafolio funcional

--- mi_web/settings.py
+++ mi_web/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -105,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
@@ -118,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- portfolio/models.py
+++ portfolio/models.py
@@ -4,10 +4,13 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(verbose_name="Título", max_length=200)
     description = models.TextField(verbose_name="Descripción")
-    image = models.ImageField(verbose_name="Imagen")
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
     created = models.DateTimeField(verbose_name="Fecha de creación", auto_now_add=True)
     updated = models.DateTimeField(verbose_name="Última actualización", auto_now=True)
 
     class Meta:
         verbose_name = "proyecto"
         ordering = ['-created']
+    
+    def __str__(self):
+        return self.title
